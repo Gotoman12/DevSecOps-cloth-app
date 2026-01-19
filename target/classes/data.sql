2d48fbb6a93410fe61a7e2a5a78af6619b8d1f67
Mslliksrjun <mallikarjunckm@example.com>
2026-01-19 11:53:18 +0530

    updated

--- target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO clothing_item (name, size, color, price, stock, category) VALUES
- ('Classic T-Shirt', 'M', 'White', 19.99, 50, 'Tops'),
- ('Classic T-Shirt', 'L', 'Black', 19.99, 40, 'Tops'),
- ('Slim Jeans', '32', 'Blue', 49.99, 30, 'Bottoms'),
- ('Hoodie', 'M', 'Gray', 39.99, 20, 'Outerwear'),
- ('Sneakers', '9', 'White', 59.99, 25, 'Shoes');
